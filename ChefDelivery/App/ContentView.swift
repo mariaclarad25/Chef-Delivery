50e09266e73466f5c041bd06dc58b3d75ba94a1d
Maria Clara <mariaclara.dg25@gmail.com>
2025-05-12 09:43:18 -0300

    Juntando elementos na página inicial

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -9,14 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(verbatim: "Teste")
+        VStack{
+            NavigationBar()
+                .padding(.horizontal, 15)
+            
+            ScrollView(.vertical, showsIndicators: false){
+                VStack{
+                    OrderTypeGridView()
+                }
+            }
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider{
-    static var previews: some View{
-        ContentView()
-            .previewLayout(.sizeThatFits)
+    
+    struct ContentView_Previews: PreviewProvider{
+        static var previews: some View{
+            ContentView()
+                .previewLayout(.sizeThatFits)
+        }
     }
 }
 /*#Preview {
