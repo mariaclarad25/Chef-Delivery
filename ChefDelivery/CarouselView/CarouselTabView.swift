02fe198ecde21c93e6ffd33cf4e6e71f367c2c25
Maria Clara <mariaclara.dg25@gmail.com>
2025-06-27 11:53:03 -0300

    Aprimorando carrosel de imagens

--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -9,21 +9,35 @@ import SwiftUI
 
 struct CarouselTabView: View {
     
+    @State private var currentIndex = 1
+    
     let ordersMock: [OrderType] = [
-    OrderType(id: 1, name: "banner burguer", image: "barbecue-banner"),
-    OrderType(id: 2, name: "banner poke", image: "pokes-banner"),
-    OrderType(id: 3, name: "banner prato", image: "brazilian-meal-banner"),
+        OrderType(id: 1, name: "banner burguer", image: "barbecue-banner"),
+        OrderType(id: 2, name: "banner poke", image: "pokes-banner"),
+        OrderType(id: 3, name: "banner prato", image: "brazilian-meal-banner"),
     ]
-                
+    
     var body: some View {
-        TabView { ForEach(ordersMock) { mock in
-                CarouselItemView(order: mock)
-                }
-            }
+        TabView (selection: $currentIndex)
+        { ForEach(ordersMock) { mock in
+            CarouselItemView(order: mock)
+                .tag(mock.id)
+        }
+        }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear{
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                withAnimation(.easeInOut(duration: 1)){
+                    if currentIndex > ordersMock.count {
+                        currentIndex = 1
+                    }
+                    currentIndex += 1
+                }
+            }
         }
     }
+}
 #Preview {
     CarouselTabView()
 }
